0474da4611c7605c0f456edcb53fc3cd34e4ab42
drh <drh@3d19cb47-1529-0410-bf6f-eb16a50b8293>
1997-06-10 00:43:21 +0000

    Clean up.

--- src/server.h
+++ src/server.h
@@ -1,58 +1,43 @@
-/* <server.h>=                                                              */
 #ifndef SERVER_INCLUDED
 #define SERVER_INCLUDED
 #include "nub.h"
 
-typedef enum {
+/* $Id$ */
+
+#define messagecodes \
+	xx(NUB_CONTINUE) \
+	xx(NUB_QUIT) \
+	xx(NUB_STARTUP) \
+	xx(NUB_BREAK) \
+	xx(NUB_FAULT) \
+	xx(NUB_SET) \
+	xx(NUB_REMOVE) \
+	xx(NUB_FETCH) \
+	xx(NUB_STORE) \
+	xx(NUB_FRAME) \
+	xx(NUB_SRC)
+
+typedef enum {		/* messages */
 #define xx(name) name,
-        /* <message codes>=                                                         */
-        xx(NUB_CONTINUE)
-        xx(NUB_QUIT)
-        xx(NUB_STARTUP)
-        xx(NUB_BREAK)
-        xx(NUB_FAULT)
-
-        /* <message codes>=                                                         */
-        xx(NUB_SET)
-
-        /* <message codes>=                                                         */
-        xx(NUB_REMOVE)
-
-        /* <message codes>=                                                         */
-        xx(NUB_FETCH)
-
-        /* <message codes>=                                                         */
-        xx(NUB_STORE)
-
-        /* <message codes>=                                                         */
-        xx(NUB_FRAME)
-
-        /* <message codes>=                                                         */
-        xx(NUB_SRC)
-
+	messagecodes
 #undef xx
 } Header_T;
 
-/* <message data structures>=                                               */
 struct nub_fetch {
-        int space;
-        void *address;
-        int nbytes;
+	int space;
+	void *address;
+	int nbytes;
 };
 
-/* <message data structures>=                                               */
 struct nub_store {
-        int space;
-        void *address;
-        int nbytes;
-        char buf[1024];
+	int space;
+	void *address;
+	int nbytes;
+	char buf[1024];
 };
 
-/* <message data structures>=                                               */
 struct nub_frame {
-        int n;
-        Nub_state_T state;
+	int n;
+	Nub_state_T state;
 };
-
-
 #endif
