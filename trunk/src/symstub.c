c569b8ff325da7049c70b2f1ea6f62da36996822
drh <drh@3d19cb47-1529-0410-bf6f-eb16a50b8293>
1997-06-10 00:37:59 +0000

    First clean-up pass; compiles.

--- trunk/src/symstub.c
+++ trunk/src/symstub.c
@@ -1,20 +1,20 @@
-/* <symstub.c>=                                                             */
 #include "symtab.h"
 #include <stdlib.h>
 #include <string.h>
 
+static char rcsid[] = "$Id$";
+
 struct ssymbol *_Sym_symbol(void *sym) { return sym; }
 struct stype *_Sym_type(void *type) { return type; }
 char *_Sym_string(void *str) { return str; }
-/* <symtab functions>=                                                      */
-struct ssymbol *_Sym_find(char *name, void *context) {
-        while (context) {
-                struct ssymbol *sym = _Sym_symbol(context);
-                if (sym->name && strcmp(name, sym->name) == 0)
-                        return sym;
-                context = sym->uplink;
-        }
-        return NULL;
+
+struct ssymbol *_Sym_find(const char *name, void *context) {
+	struct ssymbol *sym;
+
+	for ( ; context; context = sym->uplink) {
+		sym = _Sym_symbol(context);
+		if (sym->name && strcmp(name, sym->name) == 0)
+			return sym;
+	}
+	return NULL;
 }
-
-
--- trunk/src/symtab.c
+++ trunk/src/symtab.c
@@ -1,4 +1,3 @@
-/* <symtab.c>=                                                              */
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,82 +7,84 @@
 #include "symtab.h"
 #include "nub.h"
 
-char *_Sym_string(void *key) {
-        char *str;
-        static Table_T strings;
+static char rcsid[] = "$Id$";
 
-        assert(key);
-        if (strings == NULL)
-                strings = Table_new(0, 0, 0);
-        str = Table_get(strings, key);
-        if (str == NULL) {
-                char buf[2048];
-                int i = 0, n;
-                for (;;) {
-                        assert(i < sizeof buf - 4);
-                        n = _Nub_fetch(STR, (char *)key + i, &buf[i], 4);
-                        assert(n == 4);
-                        if (buf[i]) i++; else break;
-                        if (buf[i]) i++; else break;
-                        if (buf[i]) i++; else break;
-                        if (buf[i]) i++; else break;
-                }
-                str = (char *)Atom_string(buf);
-                Table_put(strings, key, str);
-        }
-        return str;
+char *_Sym_string(void *key) {
+	char *str;
+	static Table_T strings;
+
+	assert(key);
+	if (strings == NULL)
+		strings = Table_new(0, 0, 0);
+	str = Table_get(strings, key);
+	if (str == NULL) {
+		char buf[2048];
+		int i = 0, n;
+		for (;;) {
+			assert(i < sizeof buf - 4);
+			n = _Nub_fetch(STR, (char *)key + i, &buf[i], 4);
+			assert(n == 4);
+			if (buf[i]) i++; else break;
+			if (buf[i]) i++; else break;
+			if (buf[i]) i++; else break;
+			if (buf[i]) i++; else break;
+		}
+		str = (char *)Atom_string(buf);
+		Table_put(strings, key, str);
+	}
+	return str;
 }
 
 struct ssymbol *_Sym_symbol(void *sym) {
-        struct ssymbol *sp;
-        static Table_T symbols;
+	struct ssymbol *sp;
+	static Table_T symbols;
 
-        assert(sym);
-        if (symbols == NULL)
-                symbols = Table_new(0, 0, 0);
-        sp = Table_get(symbols, sym);
-        if (sp == NULL) {
-                int n;
-                NEW(sp);
-                n = _Nub_fetch(SYM, sym, sp, sizeof *sp);
-                assert(n == sizeof *sp);
-                if (sp->name)
-                        sp->name = _Sym_string(sp->name);
-                if (sp->file)
-                        sp->file = _Sym_string(sp->file);
-                Table_put(symbols, sym, sp);
-        }
-        return sp;
+	assert(sym);
+	if (symbols == NULL)
+		symbols = Table_new(0, 0, 0);
+	sp = Table_get(symbols, sym);
+	if (sp == NULL) {
+		int n;
+		NEW(sp);
+		n = _Nub_fetch(SYM, sym, sp, sizeof *sp);
+		assert(n == sizeof *sp);
+		if (sp->name)
+			sp->name = _Sym_string(sp->name);
+		if (sp->file)
+			sp->file = _Sym_string(sp->file);
+		Table_put(symbols, sym, sp);
+	}
+	return sp;
 }
 
 struct stype *_Sym_type(void *type) {
-        struct stype *ty;
-        static Table_T types;
+	struct stype *ty;
+	static Table_T types;
 
-        assert(type);
-        if (types == NULL)
-                types = Table_new(0, 0, 0);
-        ty = Table_get(types, type);
-        if (ty == NULL) {
-                int n;
-                unsigned short len;
-                n = _Nub_fetch(TYPE, type, &len, sizeof len);
-                assert(n == sizeof len);
-                ty = ALLOC(len);
-                n = _Nub_fetch(TYPE, type, ty, len);
-                assert(n == len);
-                Table_put(types, type, ty);
-        }
-        return ty;
-}
-/* <symtab functions>=                                                      */
-struct ssymbol *_Sym_find(char *name, void *context) {
-        while (context) {
-                struct ssymbol *sym = _Sym_symbol(context);
-                if (sym->name && strcmp(name, sym->name) == 0)
-                        return sym;
-                context = sym->uplink;
-        }
-        return NULL;
+	assert(type);
+	if (types == NULL)
+		types = Table_new(0, 0, 0);
+	ty = Table_get(types, type);
+	if (ty == NULL) {
+		int n;
+		unsigned short len;
+		n = _Nub_fetch(TYPE, type, &len, sizeof len);
+		assert(n == sizeof len);
+		ty = ALLOC(len);
+		n = _Nub_fetch(TYPE, type, ty, len);
+		assert(n == len);
+		Table_put(types, type, ty);
+	}
+	return ty;
 }
 
+struct ssymbol *_Sym_find(const char *name, void *context) {
+	struct ssymbol *sym;
+
+	for ( ; context; context = sym->uplink) {
+		sym = _Sym_symbol(context);
+		if (sym->name && strcmp(name, sym->name) == 0)
+			return sym;
+	}
+	return NULL;
+}
