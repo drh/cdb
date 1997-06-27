a302801cf588675707e10411216d0f1ca63b5248
drh <drh@3d19cb47-1529-0410-bf6f-eb16a50b8293>
1997-06-27 01:14:49 +0000

    Moved src/inits.c target to custom.mk; added targets for lcclib, etc. and added prereqs to clobber.

--- trunk/custom.mk
+++ trunk/custom.mk
@@ -10,8 +10,19 @@ E=.exe
 O=.obj
 A=.lib
 
-all::
+all::		$(BUILDDIR)/startup$O src/inits.c $(BUILDDIR)/liblcc$A $(BUILDDIR)/cpp$E
+
+$(BUILDDIR)/startup$O:	;	cp startup/`basename $(BUILDDIR)`$O $@
+$(BUILDDIR)/liblcc$A:	;	cp $(SRCDIR)/$@ $@
+$(BUILDDIR)/cpp$E:	;	cp $(SRCDIR)/$@ $@
 
 clobber::
-		rm -f $(BUILDDIR)/prelink.sh
-		rm -f $(BUILDDIR)/*.ilk $(BUILDDIR)/*.pdb
+		rm -f $(BUILDDIR)/prelink.sh src/inits.c $(BUILDDIR)/cpp$E $(BUILDDIR)/liblcc$A
+		rm -f $(BUILDDIR)/*.ilk $(BUILDDIR)/*.pdb $(BUILDDIR)/*.idb $(BUILDDIR)/*.pch
+		rm -f *.exe *$O *.asm *.s *.map *.pdb *.ilk *.idb
+
+src/inits.c:	$(SRCDIR)/src/inits.c makefile
+		sed '/^}/d' <$(SRCDIR)/src/inits.c >$@
+		echo '\t{extern void zstab_init(int, char *[]); zstab_init(argc, argv);}' >>$@
+		echo '}\nextern int main(int, char *[]);' >>$@
+
